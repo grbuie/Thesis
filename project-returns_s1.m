c53b718ff3efbde0fd8e77c8aa0fbe45480c0a69
grbuie <31255808+grbuie@users.noreply.github.com>
2018-03-27 14:05:25 -0400

    Update project-returns_s1.m

--- project-returns_s1.m
+++ project-returns_s1.m
@@ -1,22 +1,22 @@
-%copied and pasted data (starting at row 2, column 3 of csv file) into a matrix entitled "projects"
 [numbers,characters] = xlsread('potential_projects.xlsx','Sheet1');
 projects = numbers;
 %fund parameters
 start_year = 1;
 max_year = 10;
 proj_no = 1;
-proj_per_yr = 3;
-capital = 5000000;
+proj_per_yr = 2;
+capital = 2900000;
 %finances
 cashflow = zeros(max_year,1);
-year_ROI = zeros(max_year,1);
+fund_ROI = zeros(max_year,1);
+projects_ROI = zeros(max_year,1);
 fund_balance = zeros(max_year,1);
 returns = zeros(max_year+1,1);
+costs = zeros(max_year+1,1);
 DCF = zeros(max_year,1);
 r = .06;
-total_returns = 0;
 %cost includes 2 full time salaries and cost of operating fund
-fixed_cost = 200000;
+fixed_cost = 170000;
 
 for year  = start_year:max_year
    
@@ -25,25 +25,27 @@ for year  = start_year:max_year
    if year == 1
    capital = capital - fixed_cost; 
    cashflow(year) = -fixed_cost;
+   costs(year) = fixed_cost;
    
    for proj = 1:proj_per_yr 
       if capital > projects(proj_no,2)  
         cost = projects(proj_no,2);
         capital = capital - cost;
         cashflow(year) = cashflow(year) - cost;
+        costs(year) = costs(year) + cost;
         projects(proj_no,6) = year;
         proj_no = proj_no + 1;
       end
    end 
    
-   
    fund_balance(year) = capital;
-
+   DCF(year) = cashflow(year)/(1+r)^(year-1);
    end  
    
  %after year 1 
  if year > 1
     returns(year) = sum(projects(1:(proj_no),3));
+    costs(year) = fixed_cost;
     %project investment
     capital = capital - fixed_cost; 
     cashflow(year) = -fixed_cost;
@@ -53,6 +55,7 @@ for year  = start_year:max_year
         cost = projects(proj_no,2);
         capital = capital - cost;
         cashflow(year) = cashflow(year) - cost;
+        costs(year) = costs(year) + cost;
         projects(proj_no,6) = year;
         proj_no = proj_no + 1;
       end
@@ -61,11 +64,15 @@ for year  = start_year:max_year
        %returns
        capital = capital + returns(year);
        fund_balance(year) = capital;
-       total_returns = total_returns + returns(year);
         
        cashflow(year) = cashflow(year) + returns(year);
-       year_ROI(year) = cashflow(year)/-(cashflow(year)-returns(year));
-       DCF(year-1) = cashflow(year)/(1+r)^(year-1);
+       %roi in terms of inital fund 
+       %play with lower inital capital
+       %build in if returns in x years, don't invest in 5 years, but would
+       %hurt NPV
+       fund_ROI(year) = (sum(returns(1:year)) - sum(costs(1:year)))/capital;
+       projects_ROI(year) = (sum(returns(1:year)) - sum(costs(1:year)))/sum(costs(1:year));
+       DCF(year) = cashflow(year)/(1+r)^(year-1);
      
  end
    
@@ -74,9 +81,11 @@ end
 
 
  
-total_cost = sum(projects(1:proj_no-1,2));
-NPV = sum(DCF)- cashflow(1); 
-ROI = (total_returns - total_cost)/total_cost;
+project_cost = sum(projects(1:proj_no-1,2));
+NPV = sum(DCF); 
+total_cost = sum(costs(1:max_year));
+total_returns = sum(returns(1:max_year));
+end_fund_ROI = (total_returns - total_cost)/capital;
 
 
 % figure;
@@ -90,7 +99,7 @@ ROI = (total_returns - total_cost)/total_cost;
 % title('Yearly ROI for Revolving Fund');
 % xlabel('Year');
 % ylabel('ROI');
-% 
+% % 
 % figure;
 % plot((1:max_year),fund_balance(1:year));
 % title('Year-end Fund Balance for Revolving Fund');
